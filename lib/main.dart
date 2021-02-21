eda7378d7ef183e837e98252439f6cf3793e62e0
AgitatedMuffin <69534312+AgitatedMuffin@users.noreply.github.com>
2021-02-21 17:24:14 +0200

    Changed statusBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:dsp_student_application/Presentation/Pages/how_it_works/hiw1.dart';
 import 'package:flutter/material.dart';
 import 'package:dsp_student_application/Presentation/Theme/theme.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark));
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
