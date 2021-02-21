1160ea418d511dc54d41e25a8be3aaa8db6b4b27
AgitatedMuffin <69534312+AgitatedMuffin@users.noreply.github.com>
2021-02-21 16:54:05 +0200

    Mid Page Template

--- lib/Presentation/Pages/how_it_works/hiw1.dart
+++ lib/Presentation/Pages/how_it_works/hiw1.dart
@@ -5,23 +5,38 @@ import 'package:flutter/material.dart';
 class HIW1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final bool isPortrait = size.height > size.width ? true : false;
+
     return Scaffold(
         body: Stack(children: <Widget>[
       BackGroundGradient(),
-      ArabicImage(
-        top: -150,
-        bottom: null,
-      ),
-      ArabicImage(
-        bottom: -150,
-        top: null,
-      ),
-      // Container(
-      //   child: Image(
-      //     image: AssetImage('lib/Presentation/Images/ArabicCircle.png'),
-      //     colorBlendMode: BlendMode.lighten,
-      //   ),
-      // )
+      ArabicImage(top: -150, bottom: null, size: size.height / 1.5),
+      ArabicImage(bottom: -150, top: null, size: size.height / 1.5),
+      Positioned(
+          top: -size.height * 0.85,
+          left: -size.width * 0.4,
+          child: Container(
+            height: size.height * 2,
+            width: size.width * 1.5,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
+            ),
+          )),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            height: size.height / 5,
+          ),
+          Text(
+            'How it Works',
+            textAlign: TextAlign.center,
+            style: AppFonts.heading1,
+          )
+        ],
+      )
     ]));
   }
 }
@@ -39,7 +54,7 @@ class BackGroundGradient extends StatelessWidget {
           gradient: LinearGradient(
               begin: Alignment.bottomRight,
               end: Alignment.topLeft,
-              stops: [0.1, 1],
+              stops: [0, 1],
               colors: [AppColors.cPurple, AppColors.cGreen])),
     );
   }
@@ -48,25 +63,27 @@ class BackGroundGradient extends StatelessWidget {
 class ArabicImage extends StatelessWidget {
   final double bottom;
   final double top;
+  final double size;
 
   const ArabicImage({
     Key key,
     this.bottom,
     this.top,
+    this.size,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      left: -250,
+      left: -size / 2,
       bottom: this.bottom,
       top: this.top,
       child: BlendMask(
-        opacity: 0.8,
+        opacity: 0.5,
         blendMode: BlendMode.saturation,
         child: Image(
-          height: 600,
-          width: 600,
+          height: size,
+          width: size,
           image: AssetImage('lib/Presentation/Images/ArabicCircle.png'),
         ),
       ),
