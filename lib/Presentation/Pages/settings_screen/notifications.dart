7f3ed0e5e77aa7364e43354c012047fc08c48ef0
Habiba Ashraf <MY_NAME@example.com>
2021-02-22 06:42:42 +0200

    profile settings notifications trial1

--- lib/Presentation/Pages/settings_screen/notifications.dart
+++ lib/Presentation/Pages/settings_screen/notifications.dart
@@ -1,4 +1,7 @@
+import 'package:adobe_xd/adobe_xd.dart';
+import 'package:dsp_student_application/Presentation/Theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Notifications extends StatefulWidget {
   @override
@@ -8,12 +11,117 @@ class Notifications extends StatefulWidget {
 class _NotificationsState extends State<Notifications> {
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          
-        ),
-        ),
+      body: Stack(
+        children: [
+          Positioned(
+            right: -250,
+            top: -170,
+            child: Container(
+              width: size.height / 1.5,
+              height: size.height / 1.5,
+              child: BlendMask(
+                  blendMode: BlendMode.srcOver,
+                  opacity: 0.1,
+                  child:
+                      Image.asset('lib/Presentation/Images/ArabicCircle.png')),
+            ),
+          ),
+          Column(
+            children: [
+              SizedBox(height: 200),
+              Row(
+                children: [
+                  SizedBox(width: 32),
+                  Container(
+                    width: 24,
+                    height: 24,
+                    child: SvgPicture.asset(
+                      'lib/Presentation/Images/arrow.svg',
+                      semanticsLabel: 'Arrow',
+                      color: AppColors.cDarkGrey,
+                    ),
+                  ),
+                  SizedBox(width: 10.0),
+                  Text(
+                    'Notifications',
+                    style:
+                        AppFonts.heading2.copyWith(color: AppColors.cDarkGrey),
+                  ),
+                ],
+              ),
+              SizedBox(height: 50),
+              Row(
+                children: [
+                  SizedBox(width: 32),
+                  Container(
+                    width: size.width - 32,
+                    height: 50,
+                    child: SwitchListTile(
+                      title: Text(
+                        'Push Notifications',
+                        style: AppFonts.heading3
+                            .copyWith(color: AppColors.cDarkGrey),
+                      ),
+                      value: true,
+                      onChanged: (bool value) {
+                        setState(() {});
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              Center(
+                child: Container(
+                  height: 1,
+                  width: size.width - 50,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: [AppColors.cGreen, AppColors.cPurple]),
+                  ),
+                ),
+              ),
+              SizedBox(width: 32, height: 10),
+              Container(
+                width: size.width - 80,
+                height: 50,
+                child: SwitchListTile(
+                  title: Text(
+                    'Answered Query',
+                    style:
+                        AppFonts.appText.copyWith(color: AppColors.cDarkGrey),
+                  ),
+                  value: true,
+                  onChanged: (bool value) {
+                    setState(() {});
+                  },
+                ),
+              ),
+              SizedBox(width: 32, height: 10),
+              Container(
+                width: size.width - 80,
+                height: 50,
+                child: SwitchListTile(
+                  title: Text(
+                    'New Updates',
+                    style:
+                        AppFonts.appText.copyWith(color: AppColors.cDarkGrey),
+                  ),
+                  value: true,
+                  onChanged: (bool value) {
+                    setState(() {});
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
