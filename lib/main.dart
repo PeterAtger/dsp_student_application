0eb838cc66b53c21f2468cb0e558ecb071c5592d
AgitatedMuffin <69534312+AgitatedMuffin@users.noreply.github.com>
2021-04-04 20:51:01 +0200

    Fixed main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:dsp_student_application/Presentation/router.dart';
 import 'package:flutter/material.dart';
-import 'package:dsp_student_application/Presentation/Pages/settings_screen/settings_screen.dart';
 import 'package:dsp_student_application/Presentation/Theme/theme.dart';
 
 void main() => runApp(App());
@@ -8,32 +8,31 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Student Demo',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: AppColors.cPurple,
-          accentColor: AppColors.cGreen,
-          scaffoldBackgroundColor: Colors.white,
-          fontFamily: 'lato',
-          buttonTheme: ButtonThemeData(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(32.0))),
-          // This makes the visual density adapt to the platform that you run
-          // the app on. For desktop platforms, the controls will be smaller and
-          // closer together (more dense) than on mobile platforms.
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => Settings(),
-        });
+      title: 'Student Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: AppColors.cPurple,
+        accentColor: AppColors.cGreen,
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'lato',
+        buttonTheme: ButtonThemeData(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(32.0))),
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      onGenerateRoute: RouterGenerator.generateRoute,
+      initialRoute: '/',
+    );
   }
 }
