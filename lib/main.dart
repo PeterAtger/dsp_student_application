d399ec67f7d3025c34ec9f7badc69cd6e907609e
AgitatedMuffin <69534312+AgitatedMuffin@users.noreply.github.com>
2021-05-20 14:10:39 +0200

    Added Providers and stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:dsp_student_application/Logic/answered_questions/answeredquestions_cubit.dart';
 import 'package:dsp_student_application/Logic/internet_connection/internetconnection_cubit.dart';
 import 'package:dsp_student_application/Logic/nav_bar/navbar_cubit.dart';
+import 'package:dsp_student_application/Logic/waiting_questions/cubit/waitingquestions_cubit.dart';
+import 'package:dsp_student_application/Presentation/Pages/questions/waiting_questions.dart';
 import 'package:dsp_student_application/Presentation/router.dart';
 import 'package:flutter/material.dart';
 import 'package:dsp_student_application/Presentation/Theme/theme.dart';
@@ -24,7 +27,13 @@ class App extends StatelessWidget {
           create: (context) => NavbarCubit(),
         ),
         BlocProvider<InternetconnectionCubit>(
-            create: (context) => InternetconnectionCubit())
+            create: (context) => InternetconnectionCubit()),
+        BlocProvider<WaitingQuestionsCubit>(
+          create: (context) => WaitingQuestionsCubit(),
+        ),
+        BlocProvider<AnsweredQuestionsCubit>(
+          create: (context) => AnsweredQuestionsCubit(),
+        ),
       ],
       child: MaterialApp(
         title: 'Student Demo',
